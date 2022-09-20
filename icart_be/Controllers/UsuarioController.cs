05d348af8b7127f2eb470636d7b891e53f06d625
kevendex <kkds.3009@gmail.com>
2022-09-20 17:18:12 -0300

    .

---
+++ icart_be/Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace icart_be.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+
+        public IActionResult Infos()
+        {
+            return View();
+        }
+    }
+}

9b188398747313d70ebd6405898c02fa28802163
kevendex <kkds.3009@gmail.com>
2022-11-30 14:00:59 -0300

    .

--- icart_be/wwwroot/js/site.js
+++ icart_be/wwwroot/js/site.js
@@ -1,4 +1,25 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const cep = document.querySelector("#cep")
 
-// Write your JavaScript code.
+const showData = (result) => {
+    for (const campo in result) {
+        if (document.querySelector("#" + campo)) {
+            document.querySelector("#" + campo).value = result[campo]
+        }
+    }
+}
+
+cep.addEventListener("blur", (e) => {
+    let search = cep.value.replace("-", "")
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+
+    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+        .then(response => {
+            response.json()
+            .then(data => showData(data))
+        })
+        .catch(e => console.log('Deu erro: ' + e, message))
+})
